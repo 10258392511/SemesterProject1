ba5e067f79694e911d638d003334679a97c03f1f
Zhexin Wu <zhexwu@biwinura.ee.ethz.ch>
2021-12-10 23:14:10 +0100

    updated .sh

---
+++ scripts/run_bias_correction.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+eval "$(conda shell.bash hook)"
+conda activate deep_learning
+python bias_correction.py --input_dir /itet-stor/zhexwu/net_scratch/data/MnMs/Labeled --output_dir /itet-stor/zhexwu/net_scratch/data/MnMs_extracted
+
