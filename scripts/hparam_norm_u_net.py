6428555dc5981ba601f1a905c82c66a491ae437d
Zhexin Wu <zhexwu@biwinura.ee.ethz.ch>
2021-12-13 02:34:39 +0100

    updated sbatch file, removing blanks

--- scripts/hparam_norm_u_net.py
+++ scripts/hparam_norm_u_net.py
@@ -2,32 +2,27 @@ import subprocess
 
 
 def make_bash_script(ce, dsc, smooth_weight):
-    bash_script = f"""
-    #SBATCH --output=run_alt_norm_log/%j.out
-    #SBATCH --gres=gpu:1
-    #SBATCH --mem=40G
-    
-    eval "$(conda shell.bash hook)"
-    conda activate deep_learning
-    cd ../SemesterProject1
-    
-    if [ ! -d params ]
-    then
-        mkdir params
-    fi
-    
-    if [ ! -d images ]
-    then
-        mkdir images
-    fi
-    
-    python ./run_alternating_opt.py --dataset_name "csf" \
-    --dataset_root "/itet-stor/zhexwu/net_scratch/data/MnMs_extracted/MnMs_extracted.h5" \
-    --device "cuda" --batch_size 6 --num_workers 2 --model_save_dir "./params/norm_u_net" \
-    --eval_plot_save_dir "./images/norm_u_net" --epochs 20 --ce_weight {ce} --dsc_weight {dsc} \
-    --smooth_weight {smooth_weight}
-    """
-    # print(bash_script)
+    bash_script = f"""#!/bin/bash
+
+#SBATCH --output=run_alt_norm_log/%j.out
+#SBATCH --gres=gpu:1
+#SBATCH --mem=40G
+
+eval "$(conda shell.bash hook)"
+conda activate deep_learning
+cd ../SemesterProject1
+
+if [ ! -d params ]
+then
+    mkdir params
+fi
+
+if [ ! -d images ]
+then
+    mkdir images
+fi
+
+python ./run_alternating_opt.py --dataset_name "csf" --dataset_root "/itet-stor/zhexwu/net_scratch/data/MnMs_extracted/MnMs_extracted.h5" --device "cuda" --batch_size 6 --num_workers 2 --model_save_dir "./params/norm_u_net" --eval_plot_save_dir "./images/norm_u_net" --epochs 20 --ce_weight {ce} --dsc_weight {dsc} --smooth_weight {smooth_weight}"""
 
     return bash_script
 
@@ -41,4 +36,4 @@ if __name__ == '__main__':
             bash_script = make_bash_script(ce, dsc, smooth_weight)
             filename = f"ce_{ce}_dsc_{dsc}_s_{smooth_weight}".replace(".", "_")
             # subprocess.run(f"touch {filename}.sh", shell=True)
-            subprocess.run(f"echo {bash_script} > {filename}.sh", shell=True)
+            subprocess.run(f"echo '{bash_script}' > {filename}.sh", shell=True)
