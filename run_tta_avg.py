27283ca0949ff56f4ad4c811b20056ed6a7d6832
zhexinwu <1025839251@qq.com>
2022-02-05 09:42:57 +0100

    added bash script for tta_avg

---
+++ run_tta_avg.py
@@ -0,0 +1,101 @@
+import argparse
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pickle
+import scripts.config as config
+
+from torch.utils.data import DataLoader
+from models.modules import Normalizer
+from models.u_net import UNet
+from helpers.datasets import MnMsHDF5SimplifiedDataset, MnMs3DDataset
+from helpers.test_time_adaptation import evaluate_3D_adapt_avg
+
+
+def save_results(save_dir, dice_losses_dict, figs_curve, figs_pred):
+    # save_dir: absolute path
+    if not os.path.isdir(save_dir):
+        os.mkdir(save_dir)
+
+    with open(os.path.join(save_dir, "dice_losses.pkl"), "wb") as wf:
+        pickle.dump(dice_losses_dict, wf)
+
+    for key in figs_curve:
+        assert key in figs_pred
+        assert len(figs_curve[key]) == len(figs_pred[key])
+        for i in range(figs_curve[key]):
+            curve_path = os.path.join(save_dir, f"{key}_{i}_curve.png")
+            pred_path = os.path.join(save_dir, f"{key}_{i}_pred.png")
+            figs_curve[i].savefig(curve_path)
+            figs_pred[i].savefig(pred_path)
+
+
+if __name__ == '__main__':
+    """ 
+    python run_tta_avg.py --batch_size 2 --num_agents 5 --input_dir "data/MnMs_extracted/MnMs_extracted.h5" --input_dir_3d "data/MnMs_extracted/MnMs_extracted_3d.h5" --u_net_path "test_time_adapt_params/1643141863_9186742_batches_1_learner_20_s_0_0000_pre_train_5/u_net_epoch_9000_eval_loss_0_3041.pt" --norm_path "test_time_adapt_params/1643141863_9186742_batches_1_learner_20_s_0_0000_pre_train_5/norm_epoch_9000_eval_loss_0_3041.pt"
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--device", default="cuda")
+    parser.add_argument("--batch_size", type=int, required=True)
+    parser.add_argument("--num_agents", type=int, default=30)
+    parser.add_argument("--lr", type=float, default=3e-1)
+    parser.add_argument("--max_iters", type=int, default=50)
+    parser.add_argument("--u_net_path", required=True)
+    parser.add_argument("--norm_path", required=True)
+    parser.add_argument("--input_dir", required=True)
+    parser.add_argument("--input_dir_3d", required=True)
+    args = parser.parse_args()
+
+    DEVICE = torch.device(args.device)
+    save_root_dir_base = "test_time_adapt_results"
+    save_root_dir = os.path.join(os.getcwd(), save_root_dir_base)
+    if not os.path.isdir(save_root_dir):
+        os.mkdir(save_root_dir)  # absolute path
+
+    # load data
+    source_names = ["csf", "hvhd", "uhe"]
+    data_path = args.input_dir
+    data_path_test = args.input_dir_3d
+    train_dataset_dict = {}
+    for mode in ["train", "eval", "test"]:
+        train_dataset_dict[mode] = MnMsHDF5SimplifiedDataset(data_path, "csf", mode, transforms=[], if_augment=True,
+                                                             data_aug_dict=config.data_aug_defaults)
+    test_dataset_dict = {}
+    for source_name in source_names:
+        test_dataset_dict[source_name] = MnMs3DDataset(data_path_test, source_name, "test")
+
+    train_loader_dict = {}
+    for mode in ["train", "eval", "test"]:
+        if mode == "train":
+            shuffle = True
+        else:
+            shuffle = False
+        train_loader_dict[f"{mode}_loader"] = DataLoader(train_dataset_dict[mode], batch_size=args.batch_size,
+                                                         shuffle=shuffle)
+
+    # run 3d evaluation
+    normalizer = Normalizer(**config.normalizer_params).to(DEVICE)
+    normalizer_cp = Normalizer(**config.normalizer_params).to(DEVICE)
+    normalizer_cp.load_state_dict(torch.load(args.norm_path, map_location=DEVICE))
+    normalizer_cp.eval()
+    u_net = UNet(**config.u_net_params).to(DEVICE)
+    u_net.load_state_dict(torch.load(args.u_net_path, map_location=DEVICE))
+    u_net.eval()
+
+    kernel_sizes = [2 * i + 1 for i in range(10)]
+    augmentors = [Normalizer(kernel_size=kernel_size).to(DEVICE) for kernel_size in kernel_sizes]
+
+    normalizer_cps = [Normalizer(**config.normalizer_params).to(DEVICE) for _ in range(args.num_agents)]
+    norm_opt_cps = []
+    for normalizer_cp_iter in normalizer_cps:
+        norm_opt_cps.append(torch.optim.SGD(normalizer_cp_iter.parameters(), lr=args.lr))
+
+    losses_out, figs_out, figs_pred_out = evaluate_3D_adapt_avg(test_dataset_dict, normalizer, u_net, normalizer_cp,
+                                                                normalizer_cps, norm_opt_cps, augmentors,
+                                                                batch_size=args.batch_size, max_iters=args.max_iters,
+                                                                device=DEVICE, if_notebook=False)
+
+    directory_name = os.path.basename(os.path.dirname(args.u_net))  # timestamp_config
+    save_dir = os.path.join(save_root_dir, directory_name)  # test_time_adapt_results/timestamp_config
+    save_results(save_dir, losses_out, figs_out, figs_pred_out)
---
+++ scripts/generate_tta_avg.py
@@ -0,0 +1,63 @@
+import subprocess
+import argparse
+import os
+
+
+# The generated bash script should run in submission/
+
+
+def make_bash_script(hyper_param_dict: dict):
+    bash_script = f"""#!/bin/bash
+
+#SBATCH --output=run_alt_norm_log/%j.out
+#SBATCH --gres=gpu:1
+#SBATCH --mem=40G
+
+eval "$(conda shell.bash hook)"
+conda activate deep_learning
+cd ../SemesterProject1
+
+python run_tta_avg.py --device "cuda" --batch_size 6 --input_dir "/itet-stor/zhexwu/net_scratch/data/MnMs_extracted/MnMs_extracted.h5" --input_dir_3d "/itet-stor/zhexwu/net_scratch/data/MnMs_extracted/MnMs_extracted_3d.h5" --u_net_path f"{hyper_param_dict["u_net_path"]}" --norm_path f"{hyper_param_dict["norm_path"]}" """
+    return bash_script
+
+
+def create_filename(hyper_param_dict: dict):
+    u_net_path = hyper_param_dict["u_net_path"]  # test_time_adapt_results/timestamp_config/u_net_....pt
+    filename = os.path.basename(os.path.dirname(u_net_path)) + ".sh"  # timestamp_config.sh
+
+    return filename
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--set_num", type=int, choices=[1, 2], required=True)
+
+    args = parser.parse_args()
+    hyper_params = dict()
+    # set 1
+    dir_root = "test_time_adapt_params"
+    hyper_params[1] = [{"u_net_path": os.path.join(dir_root,
+                                                   "1642926298_7829247_batches_1_learner_10_s_0_0000_pre_train_5/u_net_epoch_9999_eval_loss_0_3086.pt"),
+                        "norm_path": os.path.join(dir_root,
+                                                  "1642926298_7829247_batches_1_learner_10_s_0_0000_pre_train_5/norm_epoch_9999_eval_loss_0_3086.pt")},
+                       {"u_net_path": os.path.join(dir_root,
+                                                   "1642926300_3914664_batches_1_learner_1_s_0_0000_pre_train_5/u_net_epoch_7600_eval_loss_0_2859.pt"),
+                        "norm_path": os.path.join(dir_root,
+                                                  "1642926300_3914664_batches_1_learner_1_s_0_0000_pre_train_5/norm_epoch_7600_eval_loss_0_2859.pt")},
+                       {"u_net_path": os.path.join(dir_root,
+                                                   "1643141863_9186742_batches_1_learner_20_s_0_0000_pre_train_5/u_net_epoch_9000_eval_loss_0_3041.pt"),
+                        "norm_path": os.path.join(dir_root,
+                                                  "1643141863_9186742_batches_1_learner_20_s_0_0000_pre_train_5/norm_epoch_9000_eval_loss_0_3041.pt")}
+                       ]
+
+    # set 2: extra
+    hyper_params[2] = []
+
+    hyper_params_list = hyper_params[args.set_num]
+    for hyper_param_dict_iter in hyper_params_list:
+        filename = create_filename(hyper_param_dict_iter)
+        bash_script = make_bash_script(hyper_param_dict_iter)
+        subprocess.run(f"echo '{bash_script}' > {filename}", shell=True)
+        # print(f"{filename}")
+        # print(f"{bash_script}")
+        # print("-" * 50)
